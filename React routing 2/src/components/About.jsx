4ab791c865e13cdad0aed5f79f89c59a66513afd
Ravi Ranjan <raviranjan1140@gmail.com>
2022-05-10 22:49:44 +0530

    react router 2 done

---
+++ React routing 2/src/components/About.jsx
@@ -0,0 +1,11 @@
+export const About=()=>{
+
+    return (
+        <div>
+            <h2>About us</h2>
+            <h1>Masai School</h1>
+
+        </div>
+    )
+
+}
---
+++ React routing 2/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export const Navbar=()=>{
+
+return (
+        <div>
+
+    <Link to="/">HOME</Link>
+    <Link to="/about">ABOUT US</Link>
+    <Link to="/users">USERS</Link>
+
+    </div>
+    )
+
+}
